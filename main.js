7321104accd78750cb08b91048a00e8bd6f3ddb8
Anthony Johnson <anthonythomasjohnson@outlook.com>
2021-10-01 13:50:48 +0900

    fixed broken code

--- main.js
+++ main.js
@@ -60,3 +60,34 @@ console.log(convertToBaby(animals))
 //------------------------------------------------------------------------------
 //------------------------------------------------------------------------------
 
+//We wrote a function, smallestPowerOfTwo(), which takes in an array.
+
+//Within our function, we create a new array called results. We then loop through the argument array and calculate the smallest power of two which is greater than the current element before using .push() to add it to results.
+
+//It’s not doing what we want. Can you fix our code, please?
+
+const numbers = [5, 3, 9, 30];
+
+const smallestPowerOfTwo = arr => {
+      let results = [];
+      // The 'outer' for loop:
+      for (let i = 0; i < arr.length; i++) {
+            number = arr[i];
+
+            // The 'inner' while loop:
+            // We needed to create a new variable!
+            let j = 1;
+            while (j < number) {
+                  j = j * 2;
+            }
+            results.push(j);
+      }
+      return results
+}
+
+console.log(smallestPowerOfTwo(numbers)) 
+
+//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
+
+
