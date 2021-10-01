feb1a2989c914460b0ba928a359e9b67cda1cd46
Anthony Johnson <anthonythomasjohnson@outlook.com>
2021-10-01 13:43:14 +0900

    convert function created

--- main.js
+++ main.js
@@ -39,3 +39,24 @@ greetAliens(aliens);
 
 //------------------------------------------------------------------------------
 //------------------------------------------------------------------------------
+
+// Write a function, convertToBaby(), that takes in an array as an argument and, using a loop, returns a new array with each string in the array prepended with 'baby '. 
+
+// Write your code here:
+const convertToBaby = (array) => {
+  const result = [];
+  for(const element of array) {
+    result.push(`baby ${element}`);
+  }
+  return result;
+}
+
+// When you're ready to test your code, uncomment the below and run:
+
+const animals = ['panda', 'turtle', 'giraffe', 'hippo', 'sloth', 'human'];
+
+console.log(convertToBaby(animals)) 
+
+//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
+
