515b80d07851c2896f12119ecbdcc6bbf1e9c78a
Anthony Johnson <anthonythomasjohnson@outlook.com>
2021-10-01 13:35:12 +0900

    reverse array function created

---
+++ main.js
@@ -0,0 +1,18 @@
+// Write a function, reverseArray(), that takes in an array as an argument and returns a new array with the elements in the reverse order. 
+// There’s a built-in method that does a lot of this work for you, but here you’re not allowed to use it. Don’t worry you’ll have plenty of opportunities to use built-in methods later on!
+
+// Write your code here:
+const reverseArray = (array) => {
+  const result = [];
+  for(const element of array) {
+    result.unshift(element);
+  }
+  return result;
+}
+
+// When you're ready to test your code, uncomment the below and run:
+ 
+const sentence = ['sense.','make', 'all', 'will', 'This'];
+
+console.log(reverseArray(sentence)) 
+// Should print ['This', 'will', 'all', 'make', 'sense.'];
